a801c845611588eed472553cf6fb1888f0268e9e
Gustavo Hendrigo Pires Santos <gustavohendrigo.gh@gmail.com>
2025-06-09 04:21:51 -0300

    Adicionando funcionalidade de captura de valores para a classe ItemPedido.

--- P2/ItemPedido.cs
+++ P2/ItemPedido.cs
@@ -6,7 +6,21 @@ using System.Threading.Tasks;
 
 namespace P2
 {
-    internal class ItemPedido
+    public class ItemPedido
     {
+        public Produto Produto { get; private set; }
+        public int Quantidade { get; private set; }
+        public decimal PrecoUnitario { get; private set; }
+        public decimal PrecoToral => Quantidade * PrecoUnitario;
+
+        public ItemPedido(Produto produto, int quantidade)
+        {
+            if (produto == null) throw new ArgumentNullException(nameof(produto), "Produto não pode ser nulo.");
+            if (quantidade <= 0) throw new ArgumentException("Quantidade tem que ser maior que 0.", nameof(quantidade));
+
+            Produto = produto;
+            Quantidade = quantidade;
+            PrecoUnitario = produto.Preco;
+        }
     }
 }

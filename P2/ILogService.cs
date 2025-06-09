44969976a29a7addb6e71a567aa5e59bcae3688b
Gustavo Hendrigo Pires Santos <gustavohendrigo.gh@gmail.com>
2025-06-09 04:55:04 -0300

    Criando a interface ILogService para mensagens informativas e mensagens de erro.

---
+++ P2/ILogService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P2
+{
+    public interface ILogService
+    {
+        void Log(string message); // Para mensagens informativas
+        void LogError(string message, Exception ex = null); // Para mensagens de erro, com opcional exceção
+    }
+}

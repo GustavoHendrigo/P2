953a7b158545929f3d2ffd0bac73687cfde9f257
Gustavo Hendrigo Pires Santos <gustavohendrigo.gh@gmail.com>
2025-06-09 12:21:40 -0300

    Adicionando a interface IDescontoService.

---
+++ P2/IDescontoService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P2
+{
+    // Interface para o serviço de aplicação de descontos
+    public interface IDescontoService
+    {
+        decimal AplicarDesconto(Pedido pedido, IEstrategiaDeDesconto estrategia);
+    }
+}

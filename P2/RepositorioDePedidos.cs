fd416c3bbbdd37c56fa99527df9aa4a803f87d2c
Gustavo Hendrigo Pires Santos <gustavohendrigo.gh@gmail.com>
2025-06-09 06:42:13 -0300

    Adicionando a classe RepositorioDePedidos para armazenar dados na memória.

---
+++ P2/RepositorioDePedidos.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P2
+{
+    public class RepositorioDePedidos : IRepositorioDePedidos
+    {
+        private readonly List<Pedido> _pedidos;
+
+        public RepositorioDePedidos()
+        {
+            _pedidos = new List<Pedido>();
+        }
+
+        public void Add(Pedido pedido)
+        {
+            _pedidos.Add(pedido);
+        }
+
+        public Pedido GetById(int id)
+        {
+            return _pedidos.FirstOrDefault(o => o.Id == id);
+        }
+
+        public IEnumerable<Pedido> GetAll()
+        {
+            return _pedidos.AsReadOnly();
+        }
+    }
+}

e6e1f051afe55ce8aceee48a5a5d5a7f66f9afb1
Gustavo Hendrigo Pires Santos <gustavohendrigo.gh@gmail.com>
2025-06-09 06:30:40 -0300

    Adicionando a interface IRepositorioDePedidos para persistência de pedidos.

---
+++ P2/IRepositorioDePedidos.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P2
+{
+    public interface IRepositorioDePedidos
+    {
+        void Add(Pedido pedido); // Adiciona um novo pedido
+        Pedido GetById(int id); // Busca um pedido pelo ID
+        IEnumerable<Pedido> GetAll(); // Retorna todos os pedidos
+    }
+}
